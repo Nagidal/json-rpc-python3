20117c416f89b6032437b001bde5064d060a1696
Nick Vincent-Maloney <nicorellius@gmail.com>
2018-04-10 16:37:54 -0700

    Remove home path from release script.

--- release.sh
+++ release.sh
@@ -3,7 +3,6 @@
 # To release, bump version in `setup.py` and add release notes in `README.rst`
 # Then run this script: `./release.sh`
 
-cd /home/nick/dev/json-rpc-python3
 python setup.py sdist
 python setup.py bdist_wheel
 twine upload dist/*
